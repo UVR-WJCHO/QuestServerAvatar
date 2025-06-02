6ba49f54c2e5ca101fecbcabaff3c150060875f1
UVR-WJCHO <woojin.cho@kaist.ac.kr>
2025-06-02 14:25:33 +0900

    .

--- mediapipeavatar/main_server.py
+++ mediapipeavatar/main_server.py
@@ -1,6 +1,7 @@
 import socket
+import sys
+import threading
 import global_vars
-
 
 class ServerUDP:
     def __init__(self, ip, port, forward_ip, forward_port):
@@ -8,6 +9,7 @@ class ServerUDP:
         self.port = port
         self.forward_ip = forward_ip
         self.forward_port = forward_port
+        self.running = True  # 프로그램 실행 상태 확인
 
         try:
             self.recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,49 +19,57 @@ class ServerUDP:
 
             print(f"Listening on {self.ip}:{self.port}")
             print(f"Forwarding received data to {self.forward_ip}:{self.forward_port}")
+
         except Exception as e:
             print(f"Error initializing server: {e}")
             sys.exit(1)  # 초기화 실패 시 종료
 
-
     def process_message(self, message):
-        # 줄 단위로 분리
         lines = message.split("\n")
 
         # 뒤에서 두 번째 줄 제거
         if len(lines) > 1:
             log_time = lines[-2]
-            del lines[-2]  # 뒤에서 두 번째 줄 삭제
+            del lines[-2]
 
-        # 수정된 메시지 재조합 및 UTF-8 인코딩
         modified_message = "\n".join(lines).encode('utf-8')
         return modified_message, log_time
 
-
-
     def start(self):
-        while True:
+        while self.running:
             try:
-                data, addr = self.recv_socket.recvfrom(65535)  # 최대 65,535 바이트 수신
+                data, addr = self.recv_socket.recvfrom(65535)
                 message = data.decode('utf-8', errors='ignore')
-                print(f"Received from {addr}")#: {message}")
+                print(f"Received from {addr}")
 
                 modified_data, log_time = self.process_message(message)
                 print("time : ", log_time)
 
-                # 수신한 데이터 그대로 127.0.0.1로 전송
                 self.send_socket.sendto(modified_data, (self.forward_ip, self.forward_port))
                 print(f"Forwarded to {self.forward_ip}:{self.forward_port}")
 
             except socket.timeout:
                 print("No data received. Server shutting down.")
-                sys.exit(1)  # 타임아웃 발생 시 종료
+                self.shutdown()
 
             except (socket.error, KeyboardInterrupt) as e:
                 print(f"Connection lost or interrupted: {e}")
-                sys.exit(1)  # 연결 끊김 발생 시 종료
+                self.shutdown()
 
+    def shutdown(self):
+        self.running = False
+        self.recv_socket.close()
+        self.send_socket.close()
+        print("Server stopped.")
+        sys.exit(0)
+
+def wait_for_exit(server):
+    input("Press Enter to stop the server...\n")
+    server.shutdown()
 
 if __name__ == "__main__":
     server = ServerUDP("0.0.0.0", global_vars.PORT, "127.0.0.1", global_vars.PORT_Unity)
+    input_thread = threading.Thread(target=wait_for_exit, args=(server,))
+    input_thread.start()
+
     server.start()
