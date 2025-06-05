baf3935113fcfdc8414fe2891c18624946ee4c6c
UVR-WJCHO <woojin.cho@kaist.ac.kr>
2025-06-05 10:29:50 +0900

    .

--- mediapipeavatar/global_vars.py
+++ mediapipeavatar/global_vars.py
@@ -12,7 +12,7 @@ HOST, PORT for main_kinect.py
 - HOST must be fixed ip
 - PORT must be allowed in ict.kaist.ac.kr for external connection
 """
-HOST = '192.168.0.5' # n5 2268 pc2 : 192.168.0.5 / n5 2325 server : 210.117.228.110
+HOST = '192.168.0.4' # n5 2268 pc2 : 192.168.0.5 / n5 2325 server : 210.117.228.110
 PORT = 52733   # test port for internal connection
 
 """
