451504595e40f80bc47a11ee53642dd2129da569
UVR-WJCHO <woojin.cho@kaist.ac.kr>
2025-06-19 10:17:04 +0900

    check kinect-Unity setting

--- mediapipeavatar/global_vars.py
+++ mediapipeavatar/global_vars.py
@@ -20,7 +20,7 @@ Port for local connection (server - Quest)
 - must be same with Port in Unity Pipeserver.cs
 """
 HOST_Unity = '127.0.0.1'  # Quest wifi ip : 192.168.0.7 , local : 127.0.0.1
-PORT_Unity = 52734
+PORT_Unity = 52735
 
 
 
